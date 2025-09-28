cce32d5cf1b9a1d667a382f4c594b0d78b865118
wudi <wicii2120@outlook.com>
2025-09-28 20:17:42 +0800

    chore: add files to package.json

--- out/src/db/helpers/columns.ts
+++
@@ -1,7 +0,0 @@
-import * as p from 'drizzle-orm/pg-core'
-
-export const timestamps = {
-  createdAt: p.timestamp({ withTimezone: true }).defaultNow(),
-  updatedAt: p.timestamp({ withTimezone: true }),
-  deletedAt: p.timestamp({ withTimezone: true }),
-}
--- out/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { env } from '../env'
-import * as schema from './schema'
-
-export const db = drizzle({
-  connection: env.PG_CONNECTION_STRING,
-  casing: 'snake_case',
-  schema,
-})
